ab1d64c5f31adca44034216ea39e22376e569b97
Dmytro Varukha <dmytro.varukha@gmail.com>
2024-05-02 13:46:31 +0300

    implemented delivery service app using spring boot

---
+++ src/main/java/org/varukha/deliveryservice/dto/vehicle/VehicleInfoDto.java
@@ -0,0 +1,7 @@
+package org.varukha.deliveryservice.dto.vehicle;
+
+/**
+ * A data transfer object (DTO) representing information about a vehicle.
+ * Includes the type of the vehicle.
+ */
+public record VehicleInfoDto(String type) {}
